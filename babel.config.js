c218b7fea3771e7ffedf2ffda2954d1b3ccba839
DaStrawBerry <hoanglonghoangmang@gmail.com>
2026-05-19 10:07:02 +0700

    fix(style): address bagel error

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = function (api) {
   api.cache(true);
-  return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: ["nativewind/babel"],
-  };
+  return { presets: ["babel-preset-expo"] };
 };
